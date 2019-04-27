7a179aeaf1c21adc051b57f938ecdeaddae073f2
James Anderson <caracallarex@gmail.com>
2019-04-27 02:24:26 +0000

    yay perl

---
+++ db/file_split.pl
@@ -0,0 +1,43 @@
+# I wrote this script because my server box is puny and couldn't handle parsing
+# the entire JMdict XML in one go.
+# And also because I just learned perl for work and I would hate for that to
+# have been a waste
+
+use strict;
+use warnings;
+
+my $input_filename = "JMdict_san";
+my $output_filename = $input_filename . "_1";
+
+open FILE, $input_filename or die $!;
+open OUTFILE, "> $output_filename" or die $!;
+
+my $file_counter = 1;
+
+# there's 181913 entries total
+my $entry_counter = 0;
+my $entries_per_file = 18200;
+
+# Need to append this to make it valid JMdict XML
+print OUTFILE "<JMdict>\n";
+
+while (<FILE>) {
+  print OUTFILE $_;
+
+  if ($_ eq "</entry>\n") {
+    $entry_counter += 1;
+
+    if ($entry_counter >= $entries_per_file) {
+      print OUTFILE "</JMdict>\n";
+      close OUTFILE;
+      $file_counter += 1;
+      $output_filename = $input_filename . "_$file_counter";
+      open OUTFILE, "> $output_filename" or die $!;
+      print OUTFILE "<JMdict>\n";
+      $entry_counter = 0;
+    }
+  }
+}
+
+print OUTFILE "</JMdict>\n";
+close OUTFILE;
