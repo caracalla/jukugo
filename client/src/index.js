a1c15c93c5c9ded2e99651f384eb4b27f74d5174
James Anderson <caracallarex@gmail.com>
2019-08-17 09:32:44 -0700

    implement routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+
 import Jukugo from "./jukugo.js";
+import { contexts } from './contexts.js';
 
 const baseUrl = 'http://api.jukugo.caracal.la';
+
+let route = (path) => {
+  switch(path) {
+    case "": {
+      return contexts.JUKUGO;
+    }
+    case "kyoushi": {
+      return contexts.KYOUSHI;
+    }
+    case "sagasu": {
+      return contexts.SAGASU
+    }
+    default: {
+      return "404";
+    }
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const domContainer = document.querySelector('#react-root');
 
-  ReactDOM.render(<Jukugo baseUrl={ baseUrl } />, domContainer);
+  let pathname = window.location.pathname.slice(1);
+
+  let context = route(pathname);
+
+  ReactDOM.render(
+    <Jukugo
+        baseUrl={ baseUrl }
+        context={ route(pathname) } />,
+    domContainer
+  );
 });
